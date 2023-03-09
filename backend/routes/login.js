db1f5ad7e0036876c6c094833556e021e7ccad7f
Vikalp Khandelwal <vikalp.khandelwal@robomq.io>
2023-03-09 10:34:40 +0530

    changed login.js

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -3,6 +3,7 @@ var router = Router();
 
 router.get('/', async(req,res) => {
     const user = await pool.query(`select * from users`)
+    console.log("hello");
     res.send("Hi "+ user[0].firstName + " Welcome to Amazon.")
 })
 
