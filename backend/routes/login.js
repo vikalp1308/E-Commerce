98b47294f75c26c4b5268b62c51c2dcb7ae81047
Vikalp Khandelwal <vikalp.khandelwal@robomq.io>
2023-03-09 10:38:19 +0530

    again hello

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -4,6 +4,7 @@ var router = Router();
 router.get('/', async(req,res) => {
     const user = await pool.query(`select * from users`)
     console.log("hello");
+    console.log('again hello')
     res.send("Hi "+ user[0].firstName + " Welcome to Amazon.")
 })
 
